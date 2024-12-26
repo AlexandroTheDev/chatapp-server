c93b90c20f99830d24aeed05359b485190197d93
Alex Meña <alex.mena@stationfive.com>
2024-12-26 10:25:34 +0800

    add dotenv

--- server.js
+++ server.js
@@ -2,25 +2,25 @@ const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 
+require("dotenv").config();
 const port = process.env.PORT || 5000;
+console.log(process.env.FE);
 
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: process.env.FE,
   },
 });
 
 io.on("connection", (socket) => {
   // console.log(socket.conn);
-  console.log("Connected");
 
   socket.on("click", (msg) => {
     console.log(msg);
   });
   socket.on("new_user", (user) => {
-    console.log(user);
     io.emit("system_message", {
       user: "system",
       message: `User ${user.name} joined the room`,
